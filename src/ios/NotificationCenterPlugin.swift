07d52bb1b3203a12659009398b8fc714b3ecd427
Alexis Kofman <alexis.kofman@gmail.com>
2015-05-26 03:45:27 +0200

    add removeObserver and removeAllObservers methods

--- src/ios/NotificationCenterPlugin.swift
+++ src/ios/NotificationCenterPlugin.swift
@@ -1,25 +1,58 @@
 import Foundation;
 
 @objc(NotificationCenterPlugin) class NotificationCenterPlugin : CDVPlugin {
-  func addObserver(command: CDVInvokedUrlCommand) {
-    let notificationCenter = NSNotificationCenter.defaultCenter();
-    let filter = command.arguments[0] as! String;
+    var allNotificationsObserver: AnyObject? = nil;
+    var addedObservers = [String: AnyObject]();
 
-    if(filter == "all"){
-      notificationCenter.addObserverForName(nil, object: nil, queue: nil) { notification in
-          self.didReceiveNotification(notification, command: command);
-      };
-    }
-    else{
-      notificationCenter.addObserverForName(filter, object: nil, queue: nil) { notification in
-          self.didReceiveNotification(notification, command: command);
-      };
-    }
-  }
+    func addObserver(command: CDVInvokedUrlCommand) {
+        let notificationCenter = NSNotificationCenter.defaultCenter();
+        let notificationName = command.arguments[0] as! String;
 
-  private func didReceiveNotification (notification: NSNotification, command: CDVInvokedUrlCommand) {
-    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: notification.name);
-    pluginResult.setKeepCallbackAsBool(true);
-    commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId);
-  }
+        if(notificationName == "all" && allNotificationsObserver == nil){
+            allNotificationsObserver = notificationCenter.addObserverForName(nil, object: nil, queue: nil) { notification in
+                self.didReceiveNotification(notification, command: command);
+            };
+        }
+
+        if(notificationName != "all"){
+            var observer = notificationCenter.addObserverForName(notificationName, object: nil, queue: nil) { notification in
+                self.didReceiveNotification(notification, command: command);
+            };
+
+            addedObservers[notificationName] = observer;
+        }
+    }
+
+    func removeObserver(command: CDVInvokedUrlCommand) {
+        let notificationCenter = NSNotificationCenter.defaultCenter();
+        let notificationName = command.arguments[0] as! String;
+
+        if(notificationName == "all"){
+            if(allNotificationsObserver != nil){
+                notificationCenter.removeObserver(allNotificationsObserver!);
+                allNotificationsObserver = nil;
+            }
+
+            if(addedObservers.count != 0){
+                for(notificationName,observer) in addedObservers{
+                    NSNotificationCenter.defaultCenter().removeObserver(observer);
+                }
+
+                addedObservers.removeAll();
+            }
+        }
+
+        if(notificationName != "all"){
+            if(addedObservers[notificationName] != nil){
+                notificationCenter.removeObserver(addedObservers[notificationName]!, name: notificationName, object: nil);
+                addedObservers.removeValueForKey(notificationName);
+            }
+        }
+    }
+
+    private func didReceiveNotification (notification: NSNotification, command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: notification.name);
+        pluginResult.setKeepCallbackAsBool(true);
+        commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId);
+    }
 }
--- www/notificationCenter.js
+++ www/notificationCenter.js
@@ -2,12 +2,12 @@ var exec = require('cordova/exec'),
     NotificationCenter = function () {};
 
 /**
- * Monitor all notifications coming from the NSNotificationCenter.
+ * Start monitoring all notifications coming from the NSNotificationCenter.
  * Do not use that in production, it's a performance killer.
  *
- * @name debug
+ * @name startDebug
  */
- NotificationCenter.prototype.debug = function () {
+ NotificationCenter.prototype.startDebug = function () {
   exec(function(notification){
         console.log(notification);
       },
@@ -16,6 +16,19 @@ var exec = require('cordova/exec'),
       "addObserver",
       ["all"]);
  };
+
+ /**
+  * Stop monitoring all notifications coming from the NSNotificationCenter.
+  *
+  * @name stopDebug
+  */
+  NotificationCenter.prototype.removeAllObservers = function () {
+   exec(null,
+       null,
+       "NotificationCenter",
+       "removeObserver",
+       ["all"]);
+  };
 
 /**
  * Observe a given notification coming from the NSNotificationCenter.
@@ -30,6 +43,21 @@ NotificationCenter.prototype.addObserver = function (notification, success, erro
    notification = [notification];
   }
   return exec(success, error, "NotificationCenter", "addObserver", notification);
+};
+
+/**
+ * Stop observing a given notification coming from the NSNotificationCenter.
+ *
+ * @name removeObserver
+ * @param {String} notification
+ * @param {Function} success callback
+ * @param {Function} error callback
+ */
+NotificationCenter.prototype.removeObserver = function (notification, success, error) {
+  if (!Array.isArray(notification)) {
+   notification = [notification];
+  }
+  return exec(success, error, "NotificationCenter", "removeObserver", notification);
 };
 
 notificationCenter = new NotificationCenter();
